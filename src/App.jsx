121b26acab1807b142ee30bfeb531f2e2dc2d43a
gelz124 <gelzrafayla@gmail.com>
2024-07-12 16:48:26 +0800

    First React Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div></div>
+      <h1>Rafayla Angelo</h1>
+      <div className="card"></div>
+      <p className="read-the-docs"></p>
+    </div>
+  );
+}
+
+export default App;
